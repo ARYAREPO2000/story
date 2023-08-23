3f9090a9c271684c126929e7f3d68cc1fd2694fd
ARYAREPO2000 <117100919+ARYAREPO2000@users.noreply.github.com>
2023-08-23 13:00:06 +0400

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text, TouchabbleOpacity, StyleSheet, Image } from 'react-native'
+
+export default class Feed extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Feed
+                </Text>
+            </View>
+        )
+    }
+}
